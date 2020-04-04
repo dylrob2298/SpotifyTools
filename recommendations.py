fe338d4e74270863af19d38aca69c675a58663a9
Dylan Robinson <dylrob123@gmail.com>
2020-04-04 14:26:57 -0400

    Update recommendations.py

--- recommendations.py
+++ recommendations.py
@@ -13,8 +13,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
 cid = "69457297a5cc42a4a9b19ba477558210"
-secret = "b8df8b59a120497eb7667bdb881058b7"
-username = "dylrobinson22"
+secret = ""
+username = ""
 
 
 def getSP():
